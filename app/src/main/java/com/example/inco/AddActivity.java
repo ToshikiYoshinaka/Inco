40a8829144c9b26450944b505af72751ff99db13
hideki <yoshihidekobaayu@gmail.com>
2020-01-16 14:51:14 +0900

    BottomButton追加

--- app/src/main/java/com/example/inco/AddActivity.java
+++ app/src/main/java/com/example/inco/AddActivity.java
@@ -1,10 +1,16 @@
 package com.example.inco;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +19,25 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+import static com.example.inco.R.id.button2;
+
 public class AddActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference();
     EditText titleEditText, contentEditText;
+
+    private static final int RESULT_PICK_IMAGEFILE = 1000;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        imageView = (ImageView)findViewById(R.id.image_view);
         titleEditText = (EditText) findViewById(R.id.title);
         contentEditText = (EditText) findViewById(R.id.content);
         // idがdialogButtonのButtonを取得
@@ -36,10 +53,22 @@ public class AddActivity extends AppCompatActivity {
                 CustomDialogFlagment dialog = new CustomDialogFlagment();
                 // 表示  getFagmentManager()は固定、sampleは識別タグ
                 dialog.show(getSupportFragmentManager(), "sample");
+
             }
         });
         //
+        findViewById(button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+
+            }
+        });
     }
+
     public void setTextView(String value){
         TextView textView = (TextView) findViewById(R.id.text);
         textView.setText(value);
@@ -62,6 +91,7 @@ public class AddActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
@@ -74,4 +104,33 @@ public class AddActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                try {
+                    Bitmap bmp = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bmp);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
 }
+
+
